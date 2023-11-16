f221297ffdfa994a24dfe9748493e8531160252e
soankusu <anak.kusuma@stud.uni-due.de>
2023-11-16 14:36:29 +0100

    Added deadline in the alertdialog for creating tasks

--- lib/models.dart
+++ lib/models.dart
@@ -1,4 +1,14 @@
 // models.dart
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart'; // used for calendar widget
+import 'package:flutter/services.dart'; //for 'FilteringTextInputFormatter in calendar and clock widget
+
+
+class Deadline {
+  DateTime? date;
+
+  Deadline(this.date);
+}
 
 class Errand {
   final String name;
@@ -12,8 +22,9 @@ class Task {
   final List<Errand> errands;
   String? file; // File associated with the task (can be a single file)
   String? link; // Link associated with the task (can be a single link)
+  Deadline? deadline; // use ? so that it can be a null value
 
-  Task(this.name, this.errands, this.file, this.link);
+  Task(this.name, this.errands, this.file, this.link, {this.deadline});
 }
 
 class Project {
@@ -22,3 +33,137 @@ class Project {
 
   Project(this.name, this.tasks);
 }
+
+
+//************* USED FOR CLOCK WIDGET *************//
+
+class TimePickerPopup extends StatefulWidget {
+  final StateSetter setState;
+
+  TimePickerPopup({required this.setState});
+
+  @override
+  _TimePickerPopupState createState() => _TimePickerPopupState();
+}
+
+class _TimePickerPopupState extends State<TimePickerPopup> {
+  TimeOfDay _selectedTime = TimeOfDay.now();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      _selectTime(context);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 300, // Adjust the width as needed
+      height: 300, // Adjust the height as needed
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          SizedBox(height: 20),
+          Text(
+            'Selected Time: ${_selectedTime.format(context)}',
+            style: TextStyle(fontSize: 20.0),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: _selectedTime,
+    );
+    if (picked != null && picked != _selectedTime) {
+      setState(() {
+        _selectedTime = picked;
+      });
+      print('Selected Time: ${_selectedTime.format(context)}'); // Print the selected time
+    }
+    Navigator.pop(context); // Close the popup after selecting the time
+  }
+}
+
+
+class TimeInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+    var newText = newValue.text;
+    if (newText.length > 0) {
+      if (newText.length >= 3) {
+        final hour = newText.substring(0, 2);
+        final minute = newText.substring(2);
+        newText = '$hour:$minute';
+      }
+    }
+    return TextEditingValue(
+      text: newText,
+      selection: TextSelection.collapsed(offset: newText.length),
+    );
+  }
+}
+//************************************************//
+
+//************* USED FOR CALENDAR WIDGET *************//
+class CalendarPopup extends StatefulWidget {
+  final StateSetter setState;
+
+  CalendarPopup({required this.setState});
+
+  @override
+  _CalendarPopupState createState() => _CalendarPopupState();
+}
+
+class _CalendarPopupState extends State<CalendarPopup> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime _selectedDay = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 500, // Adjust the width as needed
+      height: 500, // Adjust the height as needed
+      child: TableCalendar(
+        firstDay: DateTime.utc(2020, 01, 01),
+        lastDay: DateTime.utc(2030, 12, 31),
+        focusedDay: _focusedDay,
+        calendarFormat: _calendarFormat,
+        selectedDayPredicate: (day) {
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          widget.setState(() {
+            _selectedDay = selectedDay;
+            _focusedDay = focusedDay;
+          });
+        },
+      ),
+    );
+  }
+}
+
+
+class DateInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+    String newText = newValue.text;
+    if (newText.length == 5 && !newText.contains('-')) {
+      newText = '${newText.substring(0, 4)}-${newText.substring(4)}';
+    } else if (newText.length == 8 && !newText.endsWith('-')) {
+      newText = '${newText.substring(0, 4)}-${newText.substring(4, 6)}-${newText.substring(6)}';
+    }
+
+    return TextEditingValue(
+      text: newText,
+      selection: TextSelection.collapsed(offset: newText.length),
+    );
+  }
+}
+//***************************************************//
--- lib/project_list_screen.dart
+++ lib/project_list_screen.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'task_list_screen.dart';
 import 'models.dart';
+import 'package:flutter/services.dart'; //for 'FilteringTextInputFormatter in deadline functionality
 
 class ProjectListScreen extends StatefulWidget {
   @override
@@ -12,6 +13,15 @@ class ProjectListScreen extends StatefulWidget {
 class _ProjectListScreenState extends State<ProjectListScreen> {
   final TextEditingController projectNameController = TextEditingController();
   final TextEditingController singleTaskController = TextEditingController();
+  TextEditingController _dateController = TextEditingController();
+  TextEditingController _timeController = TextEditingController();
+  bool isCheckboxChecked = false;
+
+  late TimeInputFormatter _timeInputFormatter;
+  _ProjectListScreenState() {
+    // Initialize _timeInputFormatter in the constructor
+    _timeInputFormatter = TimeInputFormatter();
+  }
 
   final List<Project> projects = [
     Project('Project 1', [
@@ -175,9 +185,165 @@ class _ProjectListScreenState extends State<ProjectListScreen> {
                       builder: (BuildContext context) {
                         return AlertDialog(
                           title: Text('Create a New Task'),
-                          content: TextField(
-                            controller: singleTaskController,
-                            decoration: InputDecoration(labelText: 'Task Name'),
+                          content: SingleChildScrollView(
+                            child: StatefulBuilder(
+                              builder: (BuildContext context, StateSetter setState) {
+                                return Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    TextField(
+                                      controller: singleTaskController,
+                                      decoration:
+                                      InputDecoration(labelText: 'Task Name'),
+                                    ),
+                                    SizedBox(height: 100),
+                                    Text('Deadline'),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment
+                                          .start,
+                                      children: [
+                                        Text('Date:'),
+                                        SizedBox(width: 5),
+                                        Flexible(
+                                          child: TextField(
+                                            controller: _dateController,
+                                            decoration: InputDecoration(
+                                              labelText: 'YYYY-MM-DD',
+                                            ),
+                                            keyboardType: TextInputType
+                                                .datetime,
+                                            inputFormatters: [
+                                              FilteringTextInputFormatter
+                                                  .digitsOnly,
+                                              LengthLimitingTextInputFormatter(
+                                                  10),
+                                              // Add custom input formatter for date format
+                                              DateInputFormatter(),
+                                            ],
+                                          ),
+                                        ),
+                                        IconButton(
+                                          icon: Icon(Icons.calendar_month),
+                                          onPressed: () {
+                                            showDialog(
+                                              context: context,
+                                              builder: (BuildContext context) {
+                                                return AlertDialog(
+                                                  title: Text('Select date'),
+                                                  content: StatefulBuilder(
+                                                    builder: (
+                                                        BuildContext context,
+                                                        StateSetter setState) {
+                                                      return CalendarPopup(
+                                                          setState: setState);
+                                                    },
+                                                  ),
+                                                  actions: <Widget>[
+                                                    new TextButton(
+                                                      onPressed: () {
+                                                        Navigator.of(context)
+                                                            .pop();
+                                                      },
+                                                      child: const Text(
+                                                          'Select'),
+                                                    ),
+                                                  ],
+                                                );
+                                              },
+                                            );
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(height: 5),
+                                    Row(
+                                        mainAxisAlignment: MainAxisAlignment
+                                            .start,
+                                        children: [
+                                          Text('Time:'),
+                                          SizedBox(width: 5),
+                                          Flexible(child: TextField(
+                                            controller: _timeController,
+                                            decoration: InputDecoration(
+                                              labelText: 'Enter time (HH:MM)',
+                                            ),
+                                            keyboardType: TextInputType
+                                                .datetime,
+                                            inputFormatters: [
+                                              FilteringTextInputFormatter
+                                                  .digitsOnly,
+                                              LengthLimitingTextInputFormatter(
+                                                  4),
+                                              _timeInputFormatter,
+                                            ],
+                                          ),),
+                                          IconButton(
+                                            icon: Icon(Icons.access_time),
+                                            onPressed: () {
+                                              showDialog(
+                                                context: context,
+                                                builder: (
+                                                    BuildContext context) {
+                                                  return AlertDialog(
+                                                    title: Text('Select time'),
+                                                    content: StatefulBuilder(
+                                                      builder: (
+                                                          BuildContext context,
+                                                          StateSetter setState) {
+                                                        return TimePickerPopup(
+                                                            setState: setState);
+                                                      },
+                                                    ),
+                                                    actions: <Widget>[
+                                                      new TextButton(
+                                                        onPressed: () {
+                                                          Navigator.of(context)
+                                                              .pop();
+                                                        },
+                                                        child: const Text(
+                                                            'Select'),
+                                                      ),
+                                                    ],
+                                                  );
+                                                },
+                                              );
+                                            },
+                                          ),
+                                        ]
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text('Free deadline'),
+                                        Checkbox(
+                                          value: isCheckboxChecked,
+                                          onChanged: (value) {
+                                            setState(() {
+                                              isCheckboxChecked =
+                                                  value ?? false;
+                                            });
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                        children: [
+                                          Flexible(child: TextField(
+                                            enabled: isCheckboxChecked,
+                                          ),
+                                          ),
+                                          Text("Month"),
+                                          Flexible( child: TextField(
+                                            enabled: isCheckboxChecked,
+                                          ),
+                                          ),
+                                          Text("Day"),
+                                        ]
+                                    )
+
+                                  ],
+                                );
+                              },
+                            ),
                           ),
                           actions: <Widget>[
                             TextButton(
